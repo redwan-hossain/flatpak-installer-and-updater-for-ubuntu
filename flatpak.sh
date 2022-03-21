fab3de4cea0029fdef6d001cd0b02e02c4ff8d73
redwan-hossain <redwan.eng.cou@outlook.com>
2022-03-21 09:26:16 +0600

    pilot

---
+++ flatpak.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#ppa flatpak
+
+echo "Checking flatpak PPA status"
+
+if [[ "$(grep -iR flatpak/stable /etc/apt)" = "" ]]; then
+
+  echo " PPA missing so adding it"
+  sudo add-apt-repository ppa:flatpak/stable -y
+  sudo apt update
+  clear
+elif [[ "$(grep -iR flatpak/stable /etc/apt)" != "" ]]; then
+  echo "PPA already added"
+  sudo apt update
+  clear
+else
+  echo "Aborted"
+fi
+
+#flatpak install
+if [ "$(command -v flatpak)" = "" ] && [[ "$(grep -iR flatpak/stable /etc/apt)" != "" ]]; then
+  echo "Installing Flatpak"
+  sudo apt install flatpak -y
+  clear
+
+elif [ "$(command -v flatpak)" != "" ] && [[ "$(grep -iR flatpak/stable /etc/apt)" != "" ]]; then
+  echo "Flatpak is installed"
+
+else
+  echo "Aborted"
+fi
+
+echo "Updating and cleaning unused flatpak apps"
+flatpak uninstall --unused
+flatpak update
+flatpak list --app
---
+++ readme.md
@@ -0,0 +1,7 @@
+ # ***This script will install flatpak with official ppa and this can be reused for updating and cleaning flatpak apps***.
+
+• # Simply run `bash flatpak.sh` in the terminal.
+
+• Make sure you running the terminal from the same directory where wp.sh file is located.
+
+• *Drop a star if this script helps you*.
